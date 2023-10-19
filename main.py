6675f9083a775ef4b5fa75291657bea887ec77fa
aymannel <ayman.elamrani99@gmail.com>
2023-10-19 12:32:48 +0100

    updated Ansatz class

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import re
 import pyzx as zx
 import numpy as np
 
-from helpers import get_operator_pool
+from numbers import Real
+from typing import Literal
+from collections.abc import Sequence
 
 from openfermion import jordan_wigner, QubitOperator
 from openfermion.circuits import pauli_exp_to_qasm
 from openfermion.ops import FermionOperator
+
+from helpers import get_operator_pool
+
+
+VertexType = int
+VertexRef = int
 
 
 class Operator(FermionOperator):
@@ -43,6 +51,8 @@ class Ansatz:
         with open(f'DISCO_data/{system}/lowest.{result_id}') as file:
             data = file.read()
 
+        self.qubit_number = 8
+
         self.system: str = system
         self.result_id: str = result_id
         self.step: int = int(re.search(r'found at step\s+(\d+)', data).group(1))
@@ -55,11 +65,11 @@ class Ansatz:
 
         self.circuits: list[zx.Circuit] = self.generate_circuits()
         self.full_circuit: zx.Circuit = self.generate_full_circuit()
+        self.full_graph = self.generate_graph()
 
     def __str__(self):
-        return (f'Result: {self.result_id} \n' +
-                '\n'.join([f'({idx})    {c} π    {o}' for idx, (o, c)
-                           in enumerate(zip(self.operators, self.phases_radians))]))
+        data = enumerate(zip(self.operators, self.phases_radians))
+        return f'Result: {self.result_id} \n' + '\n'.join([f'({idx})      {c} π      {o}' for idx, (o, c) in data])
 
     def get_operators(self) -> list[Operator]:
         operator_pool = [Operator.from_fermion_operator(o) for o in get_operator_pool(system=self.system)]
@@ -87,7 +97,7 @@ class Ansatz:
             operator_strings = list(pauli_exp_to_qasm(qubit_operator, evolution_time=phase, qubit_list=None, ancilla=None))
 
             # construct zx.Circuit for a given qubit operator
-            circuit = zx.Circuit(8)
+            circuit = zx.Circuit(self.qubit_number)
             for gate in [_get_zx_gate(os) for os in operator_strings]:
                 circuit.add_gate(*gate)
             return circuit
@@ -99,3 +109,74 @@ class Ansatz:
         for circuit in self.circuits[1:]:
             complete_circuit.add_circuit(circuit)
         return complete_circuit
+
+    def generate_graph(self) -> zx.Graph:
+        def _add_vertex(g: zx.Graph, ref: VertexRef,
+                        ty: VertexType, qubit: int, row: int, phase: Real | None = None) -> VertexRef:
+            new_ref = g.add_vertex(ty, qubit=qubit, row=row, phase=phase)
+            g.add_edge((ref, new_ref))
+            return new_ref
+
+        def _pauli_gadget(g: zx.Graph,
+                          refs: Sequence[VertexRef],
+                          row: int,
+                          phase: float,
+                          ps: Sequence[tuple[int, Literal["X", "Y", "Z"]]], *,
+                          row_size: Real = 1, end_pad: Real = 1) -> tuple[Sequence[VertexRef], int]:
+            refs = list(refs)
+            assert (n:=len(refs)) >= np.max([q for q, _ in ps])+1
+
+            # 1. Change of basis to Z
+            for q, p in ps:
+                match p:
+                    case "X":
+                        refs[q] = _add_vertex(g, refs[q], ty=zx.VertexType.H_BOX, qubit=q, row=row)
+                    case "Y":
+                        refs[q] = _add_vertex(g, refs[q], ty=zx.VertexType.X, qubit=q, row=row, phase=1/2)
+
+            # 2. Phase gadget
+            for q, _ in ps:
+                refs[q] = _add_vertex(g, refs[q], ty=zx.VertexType.Z, qubit=q, row=row+1*row_size)
+            hub_ref = g.add_vertex(zx.VertexType.X, qubit=n, row=row+2*row_size)
+            for q, _ in ps:
+                g.add_edge((refs[q], hub_ref))
+            tip_ref = g.add_vertex(zx.VertexType.Z, qubit=n+1, row=row+2*row_size, phase=phase)
+            g.add_edge((hub_ref, tip_ref))
+
+            # 3. Change of basis from Z
+            for q, p in ps:
+                match p:
+                    case "X":
+                        refs[q] = _add_vertex(g, refs[q], ty=zx.VertexType.H_BOX, qubit=q, row=row+2*row_size)
+                    case "Y":
+                        refs[q] = _add_vertex(g, refs[q], ty=zx.VertexType.X, qubit=q, row=row+2*row_size, phase=-1/2)
+            return refs, row+3*row_size+end_pad
+
+        def _jw_op(g: zx.Graph,
+                   op: QubitOperator,
+                   refs: Sequence[VertexRef],
+                   row: int,
+                   angle: Real, *,
+                   row_size: Real = 1, end_pad: Real = 1) -> tuple[Sequence[VertexRef], int]:
+            for ps, c in op.terms.items():
+                phase = c.imag*angle
+                refs, row = _pauli_gadget(g, refs, row, phase, ps, row_size=row_size, end_pad=end_pad)
+            return refs, row
+
+        def ansatz_to_graph(*,
+                            ops: Sequence[int] | None = None,
+                            row_size: Real = 1, end_pad: Real = 1) -> zx.Graph:
+            if ops is None:
+                ops = range(self.qubit_number)
+            g = zx.Graph()
+            refs = [g.add_vertex(qubit=q, row=0) for q in range(self.qubit_number)]
+            row = 1
+            all_ops_list = list(zip(self.qubit_operators, self.phases_radians))
+            select_ops_list = [all_ops_list[idx] for idx in ops]
+            for op, angle in select_ops_list:
+                refs, row = _jw_op(g, op, refs, row, float(angle), row_size=row_size, end_pad=end_pad)
+            out_refs = [g.add_vertex(qubit=q, row=row) for q in range(self.qubit_number)]
+            g.add_edges(list(zip(refs, out_refs)))
+            return g
+
+        return ansatz_to_graph()
