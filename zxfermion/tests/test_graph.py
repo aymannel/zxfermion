f2aa1c30c90ac6e40653e828b7ac2ed023f38724
aymannel <ayman.elamrani99@gmail.com>
2024-03-24 12:01:14 +0000

    test day

--- zxfermion/tests/test_graph.py
+++ zxfermion/tests/test_graph.py
@@ -1,2 +1,20 @@
 # test update_num_rows()
 # test remove_wire()
+import pytest
+
+from zxfermion.graph import BaseGraph
+
+
+def test_graph():
+    graph = BaseGraph(num_qubits=4)
+    assert graph.num_qubits == 4
+    assert list(graph.inputs()) == [0, 1, 2, 3]
+    assert list(graph.outputs()) == [4, 5, 6, 7]
+    assert list(graph.edges()) == [(0, 4), (1, 5), (2, 6), (3, 7)]
+    assert graph.depth() == 2
+
+
+@pytest.parametrize('num_rows', [1, 2, 3, 4])
+def test_graph_num_rows(num_rows):
+    graph = BaseGraph(num_qubits=4, num_rows=num_rows)
+    assert graph.depth() == num_rows + 1
